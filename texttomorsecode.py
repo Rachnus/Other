3fe514ddc114e1faa22397237f8c933e92c28787
Jimi <jimi.97@hotmail.com>
2017-09-28 20:03:15 +0300

    Better version

--- texttomorsecode.py
+++ texttomorsecode.py
@@ -1,17 +1,31 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
+import sys
 
 DEBUG = 1
 
-g_fDashDelay = 0.2
-g_fDotDelay = 0.1
+# The delay after a - in seconds
+g_fDashDelay = 0.12
 
-g_fDashLength = 0.2
-g_fDotLength = 0.1
+# The delay after a . in seconds
+g_fDotDelay = 0.08
 
-g_fSpaceLength = 0.5
+# The length of a - beep in seconds
+g_fDashLength = 0.12
 
-g_cCharacterPatterns = {" ": " ",
+# The length of a . beep in seconds
+g_fDotLength = 0.08
+
+# The delay after a letter in seconds
+g_fLetterDelay = 0.1
+
+# The delay after a space (/) in seconds
+g_fSpaceDelay = 0.3
+
+g_iGPIOPin = 12
+
+# Define what letters translate to what code
+g_cCharacterPatterns = {" ": "/",
                         "A": "-.",
                         "B": "-...",
                         "C": "-.-.",
@@ -50,40 +64,49 @@ g_cCharacterPatterns = {" ": " ",
                         "0": "-----"}
 
 
+# Function to convert text to morse code
 def ConvertTextToMorse(pText):
     szMorse = ""
     for c in pText:
-        c = c.upper()
+        if c != " ":
+            c = c.upper()
         try:
-            szMorse += g_cCharacterPatterns[c]
+            szMorse += g_cCharacterPatterns[c] + " "
         except:
             print c, "does not exist in the morse code!"
-        #print g_cCharacterPatterns[c],
-    print szMorse
+            sys.exit()
+    szMorse = szMorse[:-1]
+    if DEBUG:
+        print szMorse
     return szMorse
-szMorse = ConvertTextToMorse("hello lol")
-
 
 # MAIN
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(g_iGPIOPin, GPIO.OUT)
+
+szText = "testing"
+szMorse = ConvertTextToMorse(szText)
+if DEBUG:
+    print szText
 
 for c in szMorse:
     if DEBUG:
-            print c
+        print c
 
     if c == '-':
-        GPIO.output(12, GPIO.HIGH)
+        GPIO.output(g_iGPIOPin, GPIO.HIGH)
         time.sleep(g_fDashLength)
-        GPIO.output(12, GPIO.LOW)
+        GPIO.output(g_iGPIOPin, GPIO.LOW)
         time.sleep(g_fDashDelay)
     elif c == '.':
-        GPIO.output(12, GPIO.HIGH)
+        GPIO.output(g_iGPIOPin, GPIO.HIGH)
         time.sleep(g_fDotLength)
-        GPIO.output(12, GPIO.LOW)
+        GPIO.output(g_iGPIOPin, GPIO.LOW)
         time.sleep(g_fDotDelay)
+    elif c == '/':
+        time.sleep(g_fSpaceDelay)
     else:
-        time.sleep(g_fSpaceLength)
+        time.sleep(g_fLetterDelay)
 
 GPIO.cleanup()
